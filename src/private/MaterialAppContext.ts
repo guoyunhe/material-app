30f5abd6fb0beea82479dad63f7e819129a4a91d
Guo Yunhe <i@guoyunhe.me>
2025-06-29 17:08:14 +0800

    fix(theme): not compact by default

--- src/private/MaterialAppContext.ts
+++ src/private/MaterialAppContext.ts
@@ -1,9 +1,9 @@
-import { createCompactTheme } from 'material-compact';
+import { createTheme } from '@mui/material';
 import { createContext } from 'react';
 import { MaterialAppConfig } from '../MaterialAppConfig';
 
-export const light = createCompactTheme({ palette: { mode: 'light' } });
-export const dark = createCompactTheme({ palette: { mode: 'dark' } });
+export const light = createTheme({ palette: { mode: 'light' } });
+export const dark = createTheme({ palette: { mode: 'dark' } });
 
 export const defaultConfig: MaterialAppConfig = {
   themes: { light, dark },
